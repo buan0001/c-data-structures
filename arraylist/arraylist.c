1e6edf2ee09586e2b141db433a961fc3cbbc175e
buan0001 <bustermikkel@gmail.com>
2024-09-28 16:16:45 +0200

    Started arraylist, added initial methods

---
+++ arraylist/arraylist.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct Arraylist
+{
+    /* data */
+};
+
+
+int main()
+{
+
+    return 0;
+}
+
+alst_add(objekt){}
+alst_get(index){}
+alst_size(){}
+alst_remove(){}
+alst_removeIndex(index){}
+alst_insertIndex(index, objekt){}
+alst_set(index, obj){}
+alst_clear(){}
